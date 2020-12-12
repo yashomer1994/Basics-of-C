04892fb0b84080fda1eba66a0450bba904bbad7e
Yash Omer <yashomer0007@gmail.com>
2020-12-13 01:42:23 +0530

    Addition of two integer values

---
+++ Basic-03/add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+
+	int n1, n2, sum;
+
+	printf("Enter value of two integers: ");
+	scanf("%d %d", &n1, &n2);  // Scans the value entered by user 
+
+	// Addition 
+	
+	sum = n1 + n2;
+
+	printf ("%d + %d = %d",n1, n2,sum);
+	return 0;
+}
+	
+	
+
+
