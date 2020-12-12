8cf3c40a777b42bbd3f7f741a2a3df6c074c97d9
Yash Omer <yashomer0007@gmail.com>
2020-12-13 01:31:10 +0530

    This Program indentifies values as ineteger , float or string

---
+++ Basic-02/initialize_value.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int integer = 10;
+	float fl = 5.7;
+	char string[] = "Hello,Bond007!!";
+
+	printf("%d is an integer\n",integer);
+	printf("%f is float\n",fl);
+	printf("%s is string\n",string);
+
+}
